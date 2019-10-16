4ca05d51886d150157d31057916d5f8efd538c0a
Chris Ford <christophertford@gmail.com>
2019-10-16 15:51:45 +0200

    Prep some sample messing around.

--- src/jazz/instruments.clj
+++ src/jazz/instruments.clj
@@ -5,16 +5,19 @@
 
 (definst hat []
   (let [buffer (load-sample (freesound-path 802))]
-    (play-buf 1 buffer :action FREE)))
+    (-> (play-buf 1 buffer :action FREE)
+        #_(* (env-gen (perc 0.01 0.1) :action FREE)))))
 
 (definst piano
   [note 60 level 1 rate 1 loop? 0 duration 8
    attack 0 decay 1 sustain 1 release 0.1 curve -4 gate 1]
-  (let [buf (index:kr (:id index-buffer) note)
-        env (env-gen (adsr attack decay sustain release level curve)
-                     (line:kr 1 0 duration)
-                     :action FREE)]
-    (* env (scaled-play-buf 2 buf :rate rate :level level :loop loop? :action FREE))))
+  (let [buf (index:kr (:id index-buffer) note)]
+    (-> (scaled-play-buf 2 buf :rate rate :level level :loop loop? :action FREE)
+        ;(rhpf (line:kr 1000 200 duration) 0.5)
+        ;(* 4) (clip2 0.6) (lpf 1500)
+        (* (env-gen (adsr attack decay sustain release level curve)
+                    (line:kr 1 0 duration)
+                    :action FREE)))))
 
 (defmethod play-note :default
   [{midi :pitch, seconds :duration}]
